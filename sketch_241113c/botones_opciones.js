52ebcf6fdd797b34a94ce4f1b6663d6335c63f87
Sara Eugenia Arriola Castillo <166567141+sara-arriola@users.noreply.github.com>
2024-11-17 16:03:19 -0300

    Delete sketch_241113c directory

--- sketch_241113c/botones_opciones.js
+++
@@ -1,9 +0,0 @@
-function botonOpciones(x, y, ancho, alto, texto) {
-  rectMode(CENTER);
-  fill(255);
-  rect(x, y, ancho, alto);
-  fill(0);
-  textSize(20);
-  textAlign(CENTER, CENTER);
-  text(texto, x, y);
-}
